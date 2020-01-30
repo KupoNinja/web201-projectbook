c3758949c77796779831682164c14fb923fd8e6a
Jake Overall <joverall22@gmail.com>
2020-01-29 20:56:47 -0700

    default project

--- app/store.js
+++ app/store.js
@@ -6,7 +6,10 @@ class Store {
       new Project({ id: "8", name: "P1", description: "this is p1" }),
       new Project({ id: "dfjslksa", name: "P2", description: "this is p2" })
     ],
-    activeProject: {}
+    activeProject: new Project({
+      name: "starter",
+      description: "If you click away from me I will never return"
+    })
   };
 }
 
